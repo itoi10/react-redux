498f64cfd6727bcad2208dd3b541c470a3e07d28
itict10 <itict10@gmail.com>
2021-12-07 08:18:11 +0900

    modify signup, sigin operations

--- src/reducks/users/operations.ts
+++ src/reducks/users/operations.ts
@@ -11,13 +11,25 @@ export const signIn = (email: string, password: string) => {
       return false;
     }
 
+    // メールアドレスチェック http://emailregex.com/
+    const pattern =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!pattern.test(email)) {
+      alert("メールアドレスの形式が間違っています");
+      return false
+    }
+
     // Firebaseサインイン
-    auth.signInWithEmailAndPassword(email, password).then((result) => {
-      const user = result.user;
+    auth.signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
+        // 異常系
+        if (!user) {
+          alert('ユーザー情報を取得できませんでした。')
+          return false
+        }
 
-      if (user) {
         const uid = user.uid;
-
         // Firestoreからユーザー情報取得
         firestore
           .collection("users")
@@ -36,18 +48,34 @@ export const signIn = (email: string, password: string) => {
             // トップページへ遷移
             dispatch(push("/"));
           });
-      }
-    });
+      // 異常系
+      }).catch((error) => {
+        alert('サインインに失敗しました。もう一度お試しください。' + error)
+        return false;
+      })
   };
 };
 
 // 新規ユーザー作成
 export const signUp = (username: string, email: string, password: string, confirmPassword: string) => {
   return async (dispatch: any) => {
-    // validation (more)
+    // validation
     if (username === "" || email === "" || password === "" || confirmPassword === "") {
       alert("必須項目が未入力です。");
       return false;
+    }
+
+    // メールアドレスチェック http://emailregex.com/
+    const pattern =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!pattern.test(email)) {
+      alert("メールアドレスの形式が間違っています");
+      return false
+    }
+
+    if (password.length < 6) {
+      alert("パスワードは６文字以上で入力してください。")
+      return false
     }
 
     if (password !== confirmPassword) {
@@ -56,32 +84,37 @@ export const signUp = (username: string, email: string, password: string, confir
     }
 
     // Firebaseユーザー作成
-    return auth.createUserWithEmailAndPassword(email, password).then((result) => {
-      const user = result.user;
-      if (user) {
-        const uid = user.uid;
-        const timestamp = FirebaseTimestamp.now();
+    return auth.createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
+        if (user) {
+          const uid = user.uid;
+          const timestamp = FirebaseTimestamp.now();
 
-        // 新規ユーザー情報
-        const userInitialData = {
-          uid: uid,
-          username: username,
-          email: email,
-          role: "customer",
-          created_at: timestamp,
-          updated_at: timestamp,
-        };
+          // 新規ユーザー情報
+          const userInitialData = {
+            uid: uid,
+            username: username,
+            email: email,
+            role: "customer",
+            created_at: timestamp,
+            updated_at: timestamp,
+          };
 
-        // uidをキーとしてFirestoreに新規ユーザー情報保存
-        firestore
-          .collection("users")
-          .doc(uid)
-          .set(userInitialData)
-          .then(() => {
-            // 保存が完了したらトップページに戻る
-            dispatch(push("/"));
-          });
-      }
-    });
+          // uidをキーとしてFirestoreに新規ユーザー情報保存
+          firestore
+            .collection("users")
+            .doc(uid)
+            .set(userInitialData)
+            .then(() => {
+              // 保存が完了したらトップページに戻る
+              dispatch(push("/"));
+            });
+        }
+      // 異常系
+      }).catch((error) => {
+        alert('アカウント登録に失敗しました。' + error)
+        return false;
+      })
   };
 };
