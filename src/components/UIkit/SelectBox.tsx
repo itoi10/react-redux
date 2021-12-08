57f4a93ecf46d60ead7769bc4cdf62a27063d1a9
itict10 <itict10@gmail.com>
2021-12-08 23:28:14 +0900

    add SelectBox component

---
+++ src/components/UIkit/SelectBox.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+
+interface Props {
+  label: string
+  required: boolean
+  value: string
+  select: (param: string) => void
+  options: {id:string, name:string}[]
+}
+
+const SelectBox:React.FC<Props> = (props) => {
+  return (
+    <FormControl>
+      <InputLabel>{props.label}</InputLabel>
+      <Select
+        required={props.required}
+        value={props.value}
+        onChange={(e) => props.select(e.currentTarget.value as string)}
+      >
+        {props.options.map(option => {
+          <MenuItem
+            key={option.id}
+            value={option.id}
+          >
+            {option.name}
+          </MenuItem>
+        })}
+      </Select>
+    </FormControl>
+  )
+}
+
+export default SelectBox
