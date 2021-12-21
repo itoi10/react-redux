5304ab659219343540c4fc583f7a619cf29837a3
itict10 <itict10@gmail.com>
2021-12-21 20:29:49 +0900

    add orderHistoryItem

---
+++ src/components/Products/OrderHistoryItem.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Divider } from "@material-ui/core";
+import { TextDetail } from "../UIkit";
+
+interface Props {
+  order: {
+    id: string;
+    amount: string;
+    updated_at: any;
+    shipping_date: any;
+  };
+}
+
+const datetimeToString = (date: Date) => {
+  const yyyy = date.getFullYear();
+  const MM = ("00" + (date.getMonth() + 1)).toString().slice(-2);
+  const dd = ("00" + date.getDate()).toString().slice(-2);
+  const HH = ("00" + date.getHours()).toString().slice(-2);
+  const mm = ("00" + date.getMinutes()).toString().slice(-2);
+  const ss = ("00" + date.getSeconds()).toString().slice(-2);
+  return `${yyyy}-${MM}-${dd} ${HH}:${mm}:${ss}`;
+};
+
+const dateToString = (date: Date) => {
+  const yyyy = date.getFullYear();
+  const MM = "00" + (date.getMonth() + 1).toString().slice(-2);
+  const dd = "00" + date.getDate().toString().slice(-2);
+  return `${yyyy}-${MM}-${dd}`;
+};
+
+const OrderHistoryItem: React.FC<Props> = (props) => {
+  const order = props.order;
+  const price = "¥" + order.amount.toLocaleString();
+  const orderedDatetime = datetimeToString(order.updated_at.toDate());
+  const shippingDate = dateToString(order.shipping_date.toDate());
+
+  return (
+    <div>
+      <div className="h-4" />
+      <TextDetail label={"注文ID"} value={order.id} />
+      <TextDetail label={"注文日時"} value={orderedDatetime} />
+      <TextDetail label={"発送予定日"} value={shippingDate} />
+      <TextDetail label={"注文金額"} value={price} />
+      <div className="h-2" />
+      <Divider />
+    </div>
+  );
+};
+
+export default OrderHistoryItem;
--- src/templates/OrderHistory.tsx
+++ src/templates/OrderHistory.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { List } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { getOrderHistory } from "../reducks/users/selectors";
+import { fetchOrderHistory } from "../reducks/users/operations";
+import { OrderHistoryItem } from "../components/Products";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,10 +24,19 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const OrderHistory: React.FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const selector = useSelector((state: any) => state);
+  const orders = getOrderHistory(selector);
+
+  useEffect(() => {
+    dispatch(fetchOrderHistory());
+  }, []);
 
   return (
     <section className="my-0 mx-auto max-w-xl relative py-0 px-4 text-center w-full">
-      <List className={classes.orderList}>注文履歴テスト</List>
+      <List className={classes.orderList}>
+        {orders.length > 0 && orders.map((order) => <OrderHistoryItem order={order} key={order.id} />)}
+      </List>
     </section>
   );
 };
